4802de913c0ff5bd3c93de0970dafcdffc459a68
Giancarlos Salas <giansalex@gmail.com>
2020-09-09 22:56:40 -0500

    Add jms Date/DateTime handler

---
+++ tests/Jms/XmlSchemaDateHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Greenter\Ubl\Jms;
+
+use JMS\Serializer\Context;
+use JMS\Serializer\GraphNavigator;
+use JMS\Serializer\Handler\SubscribingHandlerInterface;
+use JMS\Serializer\XmlSerializationVisitor;
+
+class XmlSchemaDateHandler implements SubscribingHandlerInterface
+{
+    public static function getSubscribingMethods()
+    {
+        return array(
+            array(
+                'direction' => GraphNavigator::DIRECTION_SERIALIZATION,
+                'format' => 'xml',
+                'type' => 'Greenter\Ubl\Entity\CommonBasic\IssueDate',
+                'method' => 'serializeDate'
+            ),
+            array(
+                'direction' => GraphNavigator::DIRECTION_SERIALIZATION,
+                'format' => 'xml',
+                'type' => 'Greenter\Ubl\Entity\CommonBasic\IssueTime',
+                'method' => 'serializeDateTime'
+            ),
+        );
+    }
+
+    public function serializeDate(XmlSerializationVisitor $visitor, \DateTime $date, array $type, Context $context)
+    {
+        $v = $date->format('Y-m-d');
+
+        return $visitor->visitSimpleString($v, $type, $context);
+    }
+
+    public function serializeDateTime(XmlSerializationVisitor $visitor, \DateTime $date, array $type, Context $context)
+    {
+        $v = $date->format('H:i:s');
+
+        return $visitor->visitSimpleString($v, $type, $context);
+    }
+}
